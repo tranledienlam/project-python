52ffdadec46e1bd22e56dcdebe15528040a7022a
TranLeDienLam <lam.tranledien@gmail.com>
2023-09-10 16:38:30 +0700

    before pull

--- web_scraping/shopee/Items.py
+++ web_scraping/shopee/Items.py
@@ -14,6 +14,14 @@ import json
 import time
 import re
 
+def config_driver():
+    options = webdriver.FirefoxOptions()
+    options.headless = True
+
+    driver = webdriver.Firefox(options=options)
+    
+    return driver
+
 class DetailCat:
     # @profile
     def __init__(self, path: str, page: int = 0) -> None:
@@ -33,6 +41,7 @@ class DetailCat:
                 options.headless = True
 
                 driver = webdriver.Firefox(options=options)
+                
                 # driver.proxy = {
                 #     'https': 'https://144.49.99.169:8080',
                 # }
@@ -58,7 +67,7 @@ class DetailCat:
                         items_data = json_data['data']['sections'][0]['data']['item']
                         
                         items = [{
-                                "itemid": i['itemid'],
+                                "itemid": i['item_card']['item']['itemid'],
                                 "name": re.sub(r'\r', '', i["name"]),
                                 "cat_itemid": i["catid"],
                                 "cat_subid": cat_subid,
@@ -171,3 +180,145 @@ class DetailCat:
         df_cats.drop_duplicates(subset=keyid, keep="last", inplace=True)
         
         return df_cats
+
+class DetailDaily:
+    # @profile
+    def __init__(self, page: int = 0) -> None:
+        """
+        page: [1-] tương đương từ trang thứ 1 đến trang N (không giới hạn)
+        """
+        self.details = None
+        
+        print(f"-- crawling Daily: page: {page} --")
+        
+        attempts = 0
+        while attempts < 2:
+            try:
+                # options = webdriver.FirefoxOptions()
+                # options.headless = True
+
+                # driver = webdriver.Firefox(options=options)
+                driver = config_driver()
+                # driver.proxy = {
+                #     'https': 'https://144.49.99.169:8080',
+                # }
+                url = f'https://shopee.vn/daily_discover?pageNumber={page}'
+
+                driver.get(url)
+
+                # Đợi cho đến khi điều kiện nào đó xuất hiện trên trang (ví dụ: element có ID 'some_element')
+                WebDriverWait(driver, 20).until(
+                    EC.presence_of_element_located((By.TAG_NAME, 'div'))
+                )
+                
+                time.sleep(3)
+                
+                for request in driver.requests:
+                    if (str(request.url)).startswith('https://shopee.vn/api/v4/homepage/get_daily_discover?bundle'):
+                        print(request.url)
+
+                        response = request.response
+                        body = decode(response.body,response.headers.get('Content-Encoding','Identity'))
+                        decode_body = body.decode('utf8')
+                        json_data = json.loads(decode_body)
+                        items_data = json_data['data']['feeds']#['item_card']['item']
+                        
+                        
+                        items = [{
+                                "itemid": i['item_card']['item']['itemid'],
+                                "name": re.sub(r'\r', '', i['item_card']['item']["name"]),
+                                "cat_itemid": i['item_card']['item']["catid"],
+                                "cat_subid": None,
+                                "price_min": str(i['item_card']['item']["price_min"])[:-5],
+                                "price_max": str(i['item_card']['item']["price_min"])[:-5],
+                                "price_min_before_discount": str(i['item_card']['item']["price_min_before_discount"])[:-5],
+                                "price_max_before_discount": str(i['item_card']['item']["price_max_before_discount"])[:-5],
+                                "discount": i['item_card']['item']["discount"],
+                                "historical_sold": i['item_card']['item']["historical_sold"],
+                                "liked_count": i['item_card']['item']["liked_count"], # số người like sản phẩm
+                                "item_rating_star": i['item_card']['item']["item_rating"]["rating_star"],
+                                "item_rating_count": i['item_card']['item']["item_rating"]["rating_count"][0],
+                                "item_rating_count_star1": i['item_card']['item']["item_rating"]["rating_count"][1],
+                                "item_rating_count_star2": i['item_card']['item']["item_rating"]["rating_count"][2],
+                                "item_rating_count_star3": i['item_card']['item']["item_rating"]["rating_count"][3],
+                                "item_rating_count_star4": i['item_card']['item']["item_rating"]["rating_count"][4],
+                                "item_rating_count_star5": i['item_card']['item']["item_rating"]["rating_count"][5],
+                                "item_rcount_with_image": i['item_card']['item']["item_rating"]["rcount_with_image"],
+                                "item_rcount_with_context": i['item_card']['item']["item_rating"]["rcount_with_context"],
+                                "ctime": datetime.fromtimestamp(i['item_card']['item']["ctime"]).strftime('%Y-%m-%d %H:%M:%S'),
+                                "shopid": i['item_card']['item']['shopid'],
+                                "shop_name": i['item_card']['item']["shop_name"],
+                                "shopee_verified": i['item_card']['item']["shopee_verified"],
+                                "shop_location": i['item_card']['item']["shop_location"],
+                                "shop_rating": i['item_card']['item']["shop_rating"],
+                            } for i in items_data]
+                        # df = pd.DataFrame(items)
+                        print(items)
+                        # print(df.head(2))
+                        # self.details = df
+                        
+            except TimeoutException:
+                # Xử lý nếu timeout xảy ra
+                print("Timeout: Không thể tải trang trong thời gian quy định 20s.")
+                break
+            except Exception as e:
+                print(f"Lỗi: {e}")
+                attempts += 1 
+                # Chờ một khoảng thời gian trước khi thử lại
+                time.sleep(20)
+            else:
+                break
+            finally:
+                driver.quit()
+    # @profile
+    def df_items(self):
+        if self.details is None:
+            return None
+        
+        df_items = self.details[[
+                            "itemid",
+                            "cat_itemid",
+                            "shopid",
+                            "name",
+                            "price_min",
+                            "price_max",
+                            "price_min_before_discount",
+                            "price_max_before_discount",
+                            "discount",
+                            "historical_sold",
+                            "liked_count",
+                            "item_rating_star",
+                            "item_rating_count",
+                            "item_rating_count_star1",
+                            "item_rating_count_star2",
+                            "item_rating_count_star3",
+                            "item_rating_count_star4",
+                            "item_rating_count_star5",
+                            "item_rcount_with_image",
+                            "item_rcount_with_context",
+                            "ctime"
+                          ]]
+        df_items = df_items.copy()
+        df_items.reset_index(drop=True, inplace=True)
+        
+        keyid = df_items.columns[0]
+        df_items.drop_duplicates(subset=keyid, keep="last", inplace=True)
+        
+        return df_items
+    # @profile
+    def df_shops(self):
+        if self.details is None:
+            return None
+        df_shops = self.details[["shopid",
+                              "shop_name",
+                              "shop_location",
+                              "shop_rating",
+                              "shopee_verified"
+                              ]]
+        
+        df_shops = df_shops.copy()
+        df_shops.reset_index(drop=True, inplace=True)
+        
+        keyid = df_shops.columns[0]
+        df_shops.drop_duplicates(subset=keyid, keep="last", inplace=True)
+        return df_shops
